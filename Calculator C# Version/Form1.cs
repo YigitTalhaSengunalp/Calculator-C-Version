6a57b558dcfa1ff26a20dd45d7c74e0d8d89bddf
Yiğit Talha Şengünalp <83014930+YigitTalhaSengunalp@users.noreply.github.com>
2021-04-24 10:21:13 +0300

    Calculator C# Version
    Just A Calculator

---
+++ Calculator C# Version/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayı1;
+            int sayı2;
+            int toplam;
+
+            sayı1 = Convert.ToInt32(textBox1.Text);
+            sayı2 = Convert.ToInt32(textBox2.Text);
+
+            toplam = sayı1 + sayı2;
+
+            label4.Text = toplam.ToString();
+        }
+    }
+}
---
+++ Calculator C# Version/README.txt
@@ -0,0 +1,3 @@
+This Is Just A Calculator
+
+If You Stole It. I Will Fuck You In Your Ass
